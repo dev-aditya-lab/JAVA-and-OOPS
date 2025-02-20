51c2b892cb4461bce4d748d9f4da77780c288f04
adii <ad1123itya@gmail.com>
2025-02-20 19:06:04 +0530

    files add

--- src/oops2/car.java
+++ src/oops2/car.java
@@ -1,10 +1,26 @@
 package oops2;
 
-//now car class is a child class of vehical class and it can access all the properties of vehical class
+//now car class is a child class of vehicle class, it can access all the properties of vehicle class
 public class car extends vehical{
+
+    @Override
+    void start() {
+//        super.start();
+        System.out.println("Car is starting");
+    }
+
+    int WheelsCount;
+
+
+
+
+
+}
+
+
+class Main{
     public static void main(String[] args) {
-       car myCarOBJ = new car();
-       myCarOBJ.WheelsCount = 4;
-       myCarOBJ.start();
+        car c = new car();
+        c.start();
     }
 }
--- src/oops2/vehical.java
+++ src/oops2/vehical.java
@@ -2,7 +2,7 @@ package oops2;
 
 public class vehical {
 
-//    these are the properties of the vehical class
+//    these are the properties of the  vehical class
     int WheelsCount;
 
     void start() {
